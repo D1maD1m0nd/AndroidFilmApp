20c4a5cc0c51ad5b04cf73c0680d3e06c104ab1d
D1ma <d1m0hkrasav4ik@gmail.com>
2021-06-17 03:54:59 +0300

    Добавление айтема для ресайкл вью, добавление модели фильма и жанра

--- app/src/main/java/com/example/filmapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/filmapp/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.filmapp.R
+import com.example.filmapp.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -14,19 +15,24 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+
+    private lateinit var binding : MainFragmentBinding
     private lateinit var viewModel: MainViewModel
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+        viewModel =  ViewModelProvider(this).get(MainViewModel :: class.java)
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
